90cd36cbfdcef042ae897df57c2b1185487db0e6
Julian Stephens <julianstephens55@gmail.com>
2020-06-03 20:54:12 -0400

    Allow unspecified save directory

--- download.py
+++ download.py
@@ -14,9 +14,10 @@ class YoutubeDownloader:
         """Create ArgumentParser object to handle command line args
         """
         self.parser = argparse.ArgumentParser(description='Extract the audio from online video(s).')
-        self.parser.add_argument('url', '-u', type=str, required=True, help='The link to your video or playlist.')
-        self.parser.add_argument('folderPath', '-f', '-d', type=str, nargs='?', 
-                                 const=self.defaultPath, help='The path to the audio destination directory.')
+        self.parser.add_argument('-u', type=str, required=True, help='The link to your video or playlist.')
+        self.parser.add_argument('-f', type=str, nargs='?', 
+                                 const=self.defaultPath, default=self.defaultPath, 
+                                 help='The path to the audio destination directory.')
         self.parser.add_argument('--playlist', '--p', type=lambda x: (str(x).lower() in ['true', '1', 'yes', 'y']), 
                                  nargs='?', default=False, help='Is this a playlist?')
      
@@ -24,14 +25,14 @@ class YoutubeDownloader:
         """Parse arguements and assign to globals
         """
         args = self.parser.parse_args()
-        self.url = args.url 
+        self.url = args.u 
 
         if args.playlist == True:
-            self.dest = args.folderPath + '%(playlist_title)s/%(title)s.%(ext)s'
+            self.dest = args.f + '%(playlist_title)s/%(title)s.%(ext)s'
         else: 
-            self.dest = args.folderPath + '%(title)s.%(ext)s'
+            self.dest = args.f + '%(title)s.%(ext)s'
 
-        print(vars(args))
+        # print(self.defaultPath)
 
     def downloadAudio(self): 
         """Download specified video or playlist and extract audio with youtube-dl
@@ -56,5 +57,5 @@ if __name__ == "__main__":
     downloader.createArgParser()
 
     downloader.getArgs()
-    #downloader.downloadAudio()
-    #print("Download complete!")
+    downloader.downloadAudio()
+    print("Download complete!")
